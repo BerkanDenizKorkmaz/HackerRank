7bb0414524e96117d4f5bfa419b5ad351d346d8d
BerkanDenizKorkmaz <bekodok27@gmail.com>
2024-02-29 20:42:33 +0300

    First Commit

---
+++ Hackerrank/Find the Runner-Up Score!.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = input().split(sep=" ", maxsplit=n - 1)
+    arr = [int(a) for a in arr]
+    arr.sort(reverse=True)
+    maxScore = arr[0]
+    print(maxScore)
+    for a in arr[1:]:
+        if not (a == maxScore):
+            print(a)
+            break
---
+++ Hackerrank/test.py
@@ -0,0 +1,7 @@
+list = []
+x = "name"
+y = 85
+list.append([x,y])
+print(list)
+list.append([78,56])
+print(list)
